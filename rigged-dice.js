56d74865473748855ddb7f074697987a294cc1a8
WarriorGoat <michael.d.spitz@gmail.com>
2022-09-20 15:09:37 -0400

    File updated Sept 20

--- rigged-dice.js
+++ rigged-dice.js
@@ -2,12 +2,12 @@ const prompt = require('prompt-sync')({sigint: true});
 
 console.log("This program creates a rigged random dice roll on a six-sided die.  The number you choose will appear twice as often as the other 5 numbers.  Please input the number you would like to be rigged to appear:");
 let n = Number(prompt(">> "));
-if (n > 0) {
-    let roll = (Math.floor(Math.random() * 7) +1);
+let roll = Math.ceil(Math.random() * 7)
+if (n > 0 && n < 7) {
     {if (roll === 7){
         console.log(`Your number is ${n}`);
     }else {
     console.log(`Your number is ${roll}`);
 }}
-}else  {console.log("Your entry is invalid please try again with a whole number.")
+} else  {console.log("Your entry is invalid please try again with a whole number from 1 - 6.")
 }
